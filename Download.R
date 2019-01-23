1cf99d9cc27fd9af3178a309caa5e98dc576c64d
Hayley Brackenridge <14hlb2@queensu.ca>
2019-01-23 11:06:22 -0500

    Install and load rentrez

---
+++ Download.R
@@ -0,0 +1,11 @@
+# BIOL432 Assingment 3
+## Hayley Brackenridge
+### Jan. 23, 2019
+
+# This script includes part one of assingment 3 which involves using the package rentrez 
+
+# install package rentrez
+install.packages("rentrez")
+# load rentrez
+library(rentrez)
+
