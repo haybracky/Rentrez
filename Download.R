02b854f63b9b8acf95c45c23d55b47043eb2ae2e
Hayley Brackenridge <14hlb2@queensu.ca>
2019-01-23 11:13:44 -0500

    Download NCBI data for 3 16S genes of Borrelia sp.

--- Download.R
+++ Download.R
@@ -9,3 +9,10 @@ install.packages("rentrez")
 # load rentrez
 library(rentrez)
 
+?entrez_fetch
+# make a vector containing NCBI accession numbers for the 16S gene of Borrelia
+ncbi_ids <- c("HQ433692.1","HQ433694.1","HQ433691.1")
+# download NCBI fasta file containing gene sequences of the above accession numbers
+Bburg<-entrez_fetch(db = "nuccore", id = ncbi_ids, rettype = "fasta")
+
+paste(Bburg)
